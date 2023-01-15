92290ae20e124bc851e9454f4b9247d6dc37ab96
logica <84759675+logica0419@users.noreply.github.com>
2023-01-16 00:12:37 +0900

    complete asdf role

---
+++ roles/asdf/library/install_node.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+# shellcheck source=/dev/null
+. "$1"
+
+if [ -z "$version" ]; then
+  echo "{\"failed\": true, \"msg\": \"Arg 'version' is Required\"}"
+  exit 1
+fi
+
+if (node -v); then
+  echo "{}"
+  exit 0
+fi
+
+if (asdf list nodejs | grep -q "$version"); then
+  asdf global nodejs "$version"
+  echo "{\"changed\":true}"
+  exit 0
+fi
+
+asdf install nodejs "$version"
+asdf global nodejs "$version"
+echo "{\"changed\":true}"
+exit 0
