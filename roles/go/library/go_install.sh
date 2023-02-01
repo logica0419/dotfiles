fe0dcc68d604f02887ec93a50a1848adc49f83e3
logica <84759675+logica0419@users.noreply.github.com>
2023-02-01 18:59:00 +0900

    created go install task

---
+++ roles/go/library/go_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+# shellcheck source=/dev/null
+. "$1"
+
+if [ -z "$name" ]; then
+  echo "{\"failed\": true, \"msg\": \"Arg 'name' is Required\"}"
+  exit 1
+fi
+
+if (type "$(basename "$name")"); then
+  echo "{}"
+  exit 0
+fi
+
+go install "$name@latest"
+
+echo "{\"changed\":true}"
