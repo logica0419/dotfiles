10500e549f9b3462982d596a073d2e0c437bd3e3
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-07-28 04:46:12 +0900

    Revert "add pixiv environment"
    This reverts commit 89d756bdd5cd1d30e21b2c814154a480776575af.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$ENV" != "server" ] && [ "$ENV" != "wsl" ] && [ "$ENV" != "pixiv" ]; then
-  echo "ENV must be set to server, wsl or pixiv"
+if [ "$ENV" != "server" ] && [ "$ENV" != "wsl" ]; then
+  echo "ENV must be set to server or wsl"
   return 1
 fi
 
