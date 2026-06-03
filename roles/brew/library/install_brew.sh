843424002735cbd2511ba608725546dfa19568d1
Takuto Nagami <logica0419@gmail.com>
2026-06-04 03:14:19 +0900

    install brew dependencies in apt role
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- roles/brew/library/install_brew.sh
+++ roles/brew/library/install_brew.sh
@@ -6,12 +6,6 @@ if command -v brew &>/dev/null; then
   exit 0
 fi
 
-case "$(uname -s)" in
-Linux)
-  sudo apt-get install build-essential procps curl file git -y
-  ;;
-esac
-
 NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "{\"changed\":true}"
